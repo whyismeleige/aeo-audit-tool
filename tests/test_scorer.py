eff28747e8d202f21215a576a7ce66df997dfb1f
whyismeleige <pjain.work@proton.me>
2026-06-13 15:21:33 +0530

    test(scorer): add meta description scoring tests

--- tests/test_scorer.py
+++ tests/test_scorer.py
@@ -1,5 +1,5 @@
 import pytest
-from app.core.scorer import _score_title
+from app.core.scorer import _score_title, _score_meta_description
 
 
 @pytest.mark.parametrize(
@@ -36,4 +36,51 @@ from app.core.scorer import _score_title
 )
 def test_score_title(title, expected):
     result = _score_title(title)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "description, expected",
+    [
+        (
+            None,
+            (
+                0,
+                [
+                    "Meta Description is not present",
+                    "Meta Description is not present in the website",
+                ],
+                [
+                    "Meta Description is required for your website",
+                    "Meta Description is heavily important for your citations",
+                ],
+            ),
+        ),
+        (
+            "Meta Description is too short",
+            (
+                10,
+                ["Meta Description is too short"],
+                [
+                    "Lengthen your Meta Description of your website.",
+                    "Make your Meta Description descriptive and definitive.",
+                ],
+            ),
+        ),
+        (
+            "Meta Description is properly defined with well definitive description which is easily understandable by Crawlers and Bots",
+            (30, ["Meta Description is properly defined"], []),
+        ),
+        (
+            "Meta Description is really too long because it more than 20 words so the score is going down for this one because excessive content disposition.",
+            (
+                20,
+                ["Meta Description is too long"],
+                ["Make the description short and consise."],
+            ),
+        ),
+    ],
+)
+def test_score_meta_description(description, expected):
+    result = _score_meta_description(description)
     assert result == expected
