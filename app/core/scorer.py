db1be60951d42664b2d01829024a5e40a0f1acbd
whyismeleige <pjain.work@proton.me>
2026-06-13 15:21:04 +0530

    feat(scorer): add meta description scoring with graduated logic and refactored title scoring

--- app/core/scorer.py
+++ app/core/scorer.py
@@ -33,8 +33,13 @@ def _score_title(title: str | None) -> tuple[int, list[str], list[str]]:
 
     word_count = len(title.split())
 
-    if word_count < 5:
-        score = round((word_count / 5) * 30)
+    upper_limit = 15
+    lower_limit = 5
+
+    max_score = 30
+    
+    if word_count < lower_limit:
+        score = round((word_count / lower_limit) * max_score)
         return (
             score,
             ["Title is too short"],
@@ -43,8 +48,52 @@ def _score_title(title: str | None) -> tuple[int, list[str], list[str]]:
                 "Make your Title descriptive and definitive",
             ],
         )
-    elif 5 <= word_count <= 15:
-        return (30, ["Title is properly defined"], [])
+    elif lower_limit <= word_count <= upper_limit:
+        return (max_score, ["Title is properly defined"], [])
     else:
-        score = max(30 - (word_count - 15) * 2, 10)
+        score = max(max_score - (word_count - upper_limit) * 2, 10)
         return (score, ["Title is too long"], ["Make the title short and consise."])
+
+
+def _score_meta_description(
+    description: str | None,
+) -> tuple[int, list[str], list[str]]:
+    if not description:
+        return (
+            0,
+            [
+                "Meta Description is not present",
+                "Meta Description is not present in the website",
+            ],
+            [
+                "Meta Description is required for your website",
+                "Meta Description is heavily important for your citations",
+            ],
+        )
+
+    word_count = len(description.split())
+
+    upper_limit = 20
+    lower_limit = 15
+    
+    max_score = 30
+
+    if word_count < lower_limit:
+        score = round((word_count / lower_limit) * max_score)
+        return (
+            score,
+            ["Meta Description is too short"],
+            [
+                "Lengthen your Meta Description of your website.",
+                "Make your Meta Description descriptive and definitive.",
+            ],
+        )
+    elif lower_limit <= word_count <= upper_limit:
+        return (max_score, ["Meta Description is properly defined"], [])
+    else:
+        score = max(max_score - (word_count - upper_limit) * 2, 10)
+        return (
+            score,
+            ["Meta Description is too long"],
+            ["Make the description short and consise."],
+        )
