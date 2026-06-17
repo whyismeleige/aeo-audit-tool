bbc3b036c353bfa061eb685cd6a24b85efa61562
whyismeleige <pjain.work@proton.me>
2026-06-17 08:53:29 +0530

    feat(jobs): create jobs table in db

---
+++ app/models/jobs.py
@@ -0,0 +1,46 @@
+import enum
+import uuid
+
+from sqlalchemy import (
+    Table,
+    Column,
+    UUID,
+    String,
+    CheckConstraint,
+    Enum,
+    DateTime,
+    MetaData,
+)
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.sql import func
+
+metadata = MetaData()
+
+
+class JobStatus(enum.Enum):
+    PENDING = "pending"
+    STARTED = "started"
+    CRAWLING = "crawling"
+    SCORING = "scoring"
+    SUCCESS = "success"
+    FAILURE = "failure"
+
+
+jobs_table = Table(
+    "jobs",
+    metadata,
+    Column("job_id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4),
+    Column("url", String(2048), nullable=False),
+    Column("status", Enum(JobStatus), nullable=False, default=JobStatus.PENDING),
+    Column("result", JSONB, nullable=True),
+    Column("error_message", String(2048), nullable=True),
+    Column("created_at", DateTime, server_default=func.now(), nullable=False),
+    Column(
+        "updated_at",
+        DateTime,
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    ),
+    Column("completed_at", DateTime, nullable=True),
+)
