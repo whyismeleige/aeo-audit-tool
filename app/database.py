59180f184c4688aeeab0d6a88ba9626182fb41b2
whyismeleige <pjain.work@proton.me>
2026-06-17 14:13:47 +0530

    feat(database): add db pooling, create new job and get job db functions

---
+++ app/database.py
@@ -0,0 +1,56 @@
+import uuid
+import asyncpg
+
+from app.logger import get_logger
+from app.config import get_settings
+from app.models.schemas import JobResponse
+
+logger = get_logger(__name__)
+settings = get_settings()
+
+db_pool = None
+
+
+async def connect():
+    global db_pool
+    if not db_pool:
+        db_pool = await asyncpg.create_pool(
+            settings.DATABASE_URL, min_size=1, max_size=5
+        )
+        logger.info("DB Pool created.")
+
+
+async def disconnect():
+    global db_pool
+    if db_pool:
+        await db_pool.close()
+        logger.info("DB pool closed.")
+
+
+async def create_job(url: str) -> str:
+    global db_pool
+
+    if not db_pool:
+        logger.error("DB pool is not initialized")
+        raise RuntimeError("DB pool is not initialized")
+
+    async with db_pool.acquire() as conn:
+        job_id = await conn.fetchval(
+            "INSERT INTO jobs (job_id, url, status) VALUES ($1, $2, $3) RETURNING job_id",
+            uuid.uuid4(), url, "PENDING"
+        )
+        return str(job_id)
+
+async def get_job(job_id: str) -> JobResponse:
+    global db_pool
+
+    if not db_pool:
+        logger.error("DB pool is not initialized")
+        raise RuntimeError("DB pool is not initialized")
+
+    async with db_pool.acquire() as conn:
+        job = await conn.fetchrow(
+            "SELECT * FROM jobs where job_id = $1",
+            uuid.UUID(job_id)
+        )    
+        return job
